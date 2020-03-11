3381f8433f0191e469fd368e0d776e85b3d7886a
Pedro Luiz <p3dr0c4@live.com>
2020-03-11 17:33:34 -0300

    padronizacao API, estruturando Modal, estilizacao incompleta do Modal

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        produtos: []
+        produtos: [],
+        produto: '',
+    },
+    filters: {
+        numeroPreco(valor) {
+            return valor.toLocaleString("pt-BR", {
+                style: "currency",
+                currency: "BRL"
+            })
+        }
     },
     methods: {
         fetchProdutos() {
             fetch("./api/produtos.json")
             .then(res => res.json())
             .then(data => this.produtos = data)
+        },
+        fetchProduto(id) {
+            fetch(`./api/produtos/${id}/dados.json`)
+            .then(res => res.json())
+            .then(data => this.produto = data)
         }
     },
     created() {
